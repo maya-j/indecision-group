abf80ccc0d88183671ebc07747ec16d5f38ad952
Maya Johnson <maya.ruth.johnson@gmail.com>
2021-09-30 16:22:52 -0400

    Update weather_gini.py

--- decision_tree_writing/weather_gini.py
+++ decision_tree_writing/weather_gini.py
@@ -1,29 +1,41 @@
+'''
+To make general:
+    - remove references to "play" as outcome
+    - generalize outcome results (could be more than 2, and not just yes/no)
+    - generalize reading in of data
+    - deal with missing data
+'''
+
+
 from tree import Tree
 from tree import Node
-from tree import Data
 import csv
+
+#attributes = []
+#atrbValues = {}
 
 '''
 Reading in the data
 '''
 def readIn(file):
     attributes = []
-    days = {}
+    data = {}
 
     with open(file, 'r') as csvfile:
         csvreader = csv.reader(csvfile)
         
         attributes = next(csvreader)
         attributes.remove("Day")
-        attributes.remove("Play?")
+        attributes.remove("class")
 
+        count = 1
         for row in csvreader:
-            day = int(row.pop(0))
-            #play = 1 if row.pop(-1)=="Yes" else 0
-            play = row.pop(-1)
-            days[day] = (row, play)
+            row.pop(0)
+            outcome = row.pop(-1)
+            data[count] = (row, outcome)
+            count += 1
 
-    return days, attributes
+    return data, attributes
 
 """
 General outline of a decision tree algorithm:
@@ -37,95 +49,171 @@ General outline of a decision tree algorithm:
 classificationName = "Play?"
 
 #identify all values of attributes
-def attributeValues(attributes, days):
+def attributeValues(attributes, data):
     atrbValues = {}
     #go through each row and identify values for attribute
     #put in dict with attribute as key (i.e. Weather, Temp, etc) with
     #  values as value (i.e sunny/cloud/rainy, hot/mild, etc)
     for atrb in attributes:
         index = attributes.index(atrb)
-        allValues = [days.get(day)[0][index] for day in days]
+        allValues = [data.get(item)[0][index] for item in data]
         atrbValues[atrb] = sorted(set(allValues))
 
     return atrbValues
 
+def outcomeValues(data):
+    outcomeValues = [data.get(item)[1] for item in data]
+    outcomeValues = sorted(set(outcomeValues))
+    return outcomeValues
 
-def split(nodeToSplit, attributes, atrbValues):
-    #identifies splits
-    #calculates info gain for each
-    #identifies best and splits on that
-    informationGain = 0
-    #for attribute in atrbValues
+
+def testSplit(nodeToSplit, atrbValues):
+    '''identifies splits
+       calculates info gain for each
+       identifies best and splits on that'''
+    informationGains = {}
+
+    for atrbName in atrbValues:
+        location = attributes.index(atrbName)
+        
+        #fills a set for each attribute value with the number of yes plays and no plays
+        values = {}
+        for value in atrbValues.get(atrbName):
+            values[value] = [0,0] #[no, yes]
+        values['Total'] = [0,0]
+
+        #calculates information gain
+        split = {}
+        for item, value in nodeToSplit.getData().items():
+            outcome = value[1]
+
+            attributeValue = value[0][location]
+            split[item] = attributeValue
+            if outcome == outcomeValues[0]:
+                values[attributeValue][0] += 1
+                values['Total'][0] += 1
+            elif outcome == outcomeValues[1]:
+                values[attributeValue][1] += 1
+                values['Total'][1] += 1
+
+        infoGain = informationGainSet(values)
+        informationGains[atrbName] = infoGain
     
+    #identifies the split with the most information gain 
+    maxGain = max(informationGains.items(), key=lambda x: x[1])
+
+    return maxGain
+    
+def makeSplit(nodeToSplit, splitOn, atrbValues):
+    location = attributes.index(splitOn)
+
+    #fills a dict with the attribute values and the days with that value
+    splits = {}
+    for atrbValue in atrbValues[splitOn]:
+        splits[atrbValue] = {}
+    for item, value in nodeToSplit.getData().items():
+        attributeValue = value[0][location]
+        splits[attributeValue][item] = value        
+
+    #remove attributeValues with no days in them
+    splits2 = {atrbValue: value for atrbValue, value in splits.items() if value != {}}
+
+    #fill children of current node with splits
+    #count = 1
+    for atrbValue, value in splits2.items():
+        child = Node(value, nodeToSplit.getLevel()+1)
+        child.setSplitOn((splitOn, atrbValue))
+        nodeToSplit.addChild(child)
+        '''if (count == 1):
+            nodeToSplit.setLeft(Node(value, nodeToSplit.getLevel()+1))
+            nodeToSplit.getLeft().setSplitOn((splitOn, atrbValue))
+        elif (count == 2):
+            nodeToSplit.setRight(Node(value, nodeToSplit.getLevel()+1))
+            nodeToSplit.getRight().setSplitOn((splitOn, atrbValue))
+        elif (count == 3):
+            nodeToSplit.setCenter(Node(value, nodeToSplit.getLevel()+1))  
+            nodeToSplit.getCenter().setSplitOn((splitOn, atrbValue))      
+        count += 1'''
+    return
+
 
 """
 The math things
 General idea?: find all potential splits -> run information gain on each -> info gain calls gini impurity
 """
 
-#general idea - can customize to data structures later
-#parameters: parent set, sets of children?
-def informationGain(parent):
-    #EDIT pass parent to giniImpurity
-    parentGini = giniImpurity(parent)
+def informationGainSet(valueSet):
+    #gets parent gini impurity
+    parentGini = giniImpuritySet(valueSet['Total'])
     #initialize these variables
     childrenTotalSum = 0
     avgChildrenTotal = 0
 
-    numChildren = (2 if parent.center == None else 3)
-
-    for child in numChildren:
+    valueSet.pop("Total")
+    for child in valueSet:
         #do gini impurity for each
-        childrenTotalSum += giniImpurity()
-    avgChildrenTotal = childrenTotalSum / numChildren
+        if (valueSet[child][0] + valueSet[child][1]) != 0:
+            childrenTotalSum += giniImpuritySet(valueSet[child])
+    avgChildrenTotal = childrenTotalSum / len(valueSet)
+    return parentGini - avgChildrenTotal
 
-    
+
 #calculates the gini impurity for a single node
-def giniImpurity(datanode):
-    totalNum = datanode.getNumDays()
-
-    play = []
-    yesplay = 0
-    noplay = 0
-    
-    for day in datanode.getDays():
-        if (datanode.getDays().get(day)[1] == 1):
-            yesplay += 1
-        else:
-            noplay += 1
-
-    print("Yes: ", yesplay , ", No: " , noplay, ", total days: ", totalNum)
-        
-    giniImpurity = 1 - (yesplay/totalNum)**2 - (noplay/totalNum)**2
-    print(giniImpurity)
+def giniImpuritySet(set):
+    #set of form [no, yes]
+    totalNum = set[0] + set[1]
+    print(totalNum)
+    giniImpurity = 1 - (set[0]/totalNum)**2 - (set[1]/totalNum)**2
     
     return giniImpurity
 
 
+'''Checks all possible values to split on and makes the best one'''
+def fillLevel(node, atrbValues):
+    if(node == None):
+        return
+
+    splitOn, infoGain = testSplit(node, atrbValues)
+
+    if(infoGain == 0.0):
+        return
+
+    makeSplit(node, splitOn, atrbValues)
+
+    atrbValues.pop(splitOn)
+    #if no new attributes to split on, return as leaf node
+    if atrbValues == {}:
+        return
+    
+    for child in node.getChildren():
+        fillLevel(child, atrbValues)
+    #fillLevel(node.getRight(), atrbValues)
+    #fillLevel(node.getCenter(), atrbValues)
+
 
 def main():
     print("Starting decision tree making")
-    days, attributes = readIn('weather_play.csv')
+    global attributes
+    #data, attributes = readIn('agaricus-lepiota.csv')
+    data, attributes = readIn('weather_play.csv')
+    global outcomeValues
+    outcomeValues = outcomeValues(data)
+    #print(data)
 
-    #print('Attribute names are: ' + ', '.join(attribute for attribute in attributes))
-    #print(days)
-    #print('\n')
-
-    
     ourTree = Tree()
-    root = Node(days, attributes)
-    ourTree.root = root
+    root = Node(data)
+    ourTree.setRoot(root)
+    ourTree.getRoot().setSplitOn("Root")
 
-    #print(ourTree.root)
-    giniImpurity(ourTree.getRoot())    
-    atrbValues = attributeValues(attributes, days)
-    print(atrbValues)
-    print(ourTree.getRoot().getDays())
+    atrbValues = attributeValues(attributes, data)
+    #print(atrbValues)
+    #print(attributes)
+
+    fillLevel(ourTree.getRoot(), atrbValues)
+    print()
+    ourTree.printTree()
     
 
 
 if __name__ == "__main__":
     main()
-
-
-
