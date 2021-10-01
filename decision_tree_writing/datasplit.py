73219b9c4eabbbc30397143e01b1deb26559a786
rosie <rosiearasa@wc119-dhcp-123.brynmawr.edu>
2021-10-01 14:27:04 -0400

    split, data

---
+++ decision_tree_writing/datasplit.py
@@ -0,0 +1,13 @@
+'''using scikit learn train test split 
+to split the data into training set and test set
+'''
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+sklearn.model_selection.train_test_split(**arrays, **options) ->list
+
+#get the data in the format it appears
+#define the 80-20 range for training and text data
+x_train, x_test, y_train, y_test = train_test_split(#insert data, #insert data)
+
+
